fb050a603c4eff3380b95b34c035ac3053a0c781
DevilDeva08 <dalsingsaraibachpan@gmail.com>
2025-02-21 12:41:01 +0530

    Update start.py

--- INNOCENTBOTS/modules/start.py
+++ INNOCENTBOTS/modules/start.py
@@ -5,12 +5,12 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_BUTTON = [
     [
-        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/RUHI_X_MUSICBOT"),
-        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/its_innocent_boy_8202")
+        Button.url(" 𝐌ᴜsɪᴄ ", "https://t.me/SUKOONN_MUSIC_BOT"),
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/its_deva_heree")
     ],
     [
-        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/THE_FUCKING_BOT_2926"),
-        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/THE_FUCKER_BOTS_2926")
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/TFW_NETWORK"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/TFW_BOTS_SUPPORT")
     ],
 ]
 
@@ -33,7 +33,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/c6abb12b29b471031ace1.jpg",
+            "https://files.catbox.moe/m37bvo.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
